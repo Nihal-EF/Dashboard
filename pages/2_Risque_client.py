584a27c9b03eec02dca045397c1220610a8d5547
Nihal-EF <elfahim.nihal@gmail.com>
2022-07-28 14:22:53 +0200

    dev : sections of dashboard

---
+++ pages/2_Risque_client.py
@@ -0,0 +1,201 @@
+import pandas as pd
+import streamlit as st
+import requests
+from datetime import date
+from datetime import datetime, timedelta
+from streamlit_echarts import st_echarts
+import js2py
+from PIL import Image
+
+
+def request_prediction(model_uri, data):
+    headers = {"Content-Type": "application/json"}
+
+    data_json = {'columns':["EXT_SOURCE_1","EXT_SOURCE_2","DAYS_EMPLOYED", "DAYS_BIRTH", "DAYS_ID_PUBLISH", "PAYMENT_RATE", "ANNUITY_INCOME_PERC", "DPD_BOOL"], 'data': data}
+    response = requests.request(
+        method='POST', headers=headers, url=model_uri, json=data_json)
+
+    if response.status_code != 200:
+        raise Exception(
+            "Request failed with status {}, {}".format(response.status_code, response.text))
+
+    return response.json()
+
+if 'client_nouveau' not in st.session_state:
+    st.session_state['client_nouveau'] = True
+if 'client_existant' not in st.session_state:
+    st.session_state['client_existant'] = False
+    
+def main():
+    MLFLOW_URI = 'http://127.0.0.1:5000/invocations'
+    st.set_page_config(layout="wide")
+    test_data = pd.read_csv("test_df_dropna.csv")
+    train_data = pd.read_csv("train_df_dropna.csv")
+    if (st.session_state['client_nouveau']):
+        
+
+        
+        st.title('Scoring crédit')
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            
+            EXT_SOURCE_1 = st.number_input('Données source extérieur 1',
+                                     min_value=0., value=.5, step=.1)
+            EXT_SOURCE_2 = st.number_input('Données source extérieur 2',
+                                     min_value=0., value=.5, step=.1)
+            st.write('Retard sur des crédits précédents')
+            DPD_TRUE= st.checkbox('oui')
+                                         
+        with col2:
+
+            DATE_EMPLOYED = st.date_input('Date de début d\'emploi')
+            DATE_BIRTH = st.date_input('Date de naissance', min_value=datetime.strptime("01/01/1950", "%d/%m/%Y"), max_value=date.today())
+            DATE_ID_PUBLISH = st.date_input('Date d''édition de la pièce d\’identité')
+        with col3 :
+            PAYMENT_RATE =st.slider('Taux de remboursement',
+                                 min_value=1, max_value = 20, value=5, step=1)/100
+
+
+            ANNUITY_INCOME_PERC = st.slider('Taux d\'endettement',
+                             min_value=1, max_value = 35, value=5, step=1)/100
+        today = date.today()
+        DAYS_EMPLOYED = (today - DATE_EMPLOYED).days
+        DAYS_BIRTH = (today - DATE_BIRTH).days
+        DAYS_ID_PUBLISH = (today - DATE_ID_PUBLISH).days
+        DPD_BOOL = DPD_TRUE
+        data = [[EXT_SOURCE_1,EXT_SOURCE_2,DAYS_EMPLOYED, DAYS_BIRTH, DAYS_ID_PUBLISH, PAYMENT_RATE, ANNUITY_INCOME_PERC, DPD_BOOL]]
+    else:
+        SK_ID_CURR = st.selectbox(
+            'Identifiant du client :', test_data["SK_ID_CURR"])
+        data = test_data[test_data["SK_ID_CURR"]==SK_ID_CURR][["EXT_SOURCE_1","EXT_SOURCE_2","DAYS_EMPLOYED", "DAYS_BIRTH", "DAYS_ID_PUBLISH", "PAYMENT_RATE", "ANNUITY_INCOME_PERC", "DPD_BOOL"]].values
+        DATE_EMPLOYED = (date.today()+timedelta(int(data[0][2])))
+        DATE_BIRTH = (date.today()+timedelta(int(data[0][3])))
+        DATE_ID_PUBLISH = (date.today()+timedelta(int(data[0][4])))
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            
+            EXT_SOURCE_1= st.number_input('Données source extérieur 1',
+                                     min_value=0., value=data[0][0], step=.1)
+            EXT_SOURCE_2 = st.number_input('Données source extérieur 2',
+                                     min_value=0., value=data[0][1], step=.1)
+            st.write('Retard sur des crédits précédents')
+            DPD_TRUE= st.checkbox('oui', value = data[0][7])
+                                         
+        with col2:
+            DATE_EMPLOYED = st.date_input('Date de début d\'emploi', value = DATE_EMPLOYED)
+            DATE_BIRTH = st.date_input('Date de naissance', min_value=datetime.strptime("01/01/1950", "%d/%m/%Y"), max_value=date.today(), value = DATE_BIRTH)
+            DATE_ID_PUBLISH = st.date_input('Date d''édition de la pièce d\’identité', value = DATE_ID_PUBLISH)
+        with col3 :
+            PAYMENT_RATE =st.slider('Taux de remboursement',
+                                 min_value=1, max_value = 20, value=int(data[0][5]*100), step=1)
+
+
+            ANNUITY_INCOME_PERC = st.slider('Taux d\'endettement',
+                             min_value=1, max_value = 35, value=int(data[0][6]*100), step=1)
+        today = date.today()
+        DAYS_EMPLOYED = (today - DATE_EMPLOYED).days
+        DAYS_BIRTH = (today - DATE_BIRTH).days
+        DAYS_ID_PUBLISH = (today - DATE_ID_PUBLISH).days
+        DPD_BOOL = DPD_TRUE
+        data = [[EXT_SOURCE_1,EXT_SOURCE_2,DAYS_EMPLOYED, DAYS_BIRTH, DAYS_ID_PUBLISH, PAYMENT_RATE, ANNUITY_INCOME_PERC, DPD_BOOL]]
+        
+    predict_btn = st.button('Prédire')
+    st.session_state['data'] = data
+    
+    if predict_btn:
+        
+        pred = None
+        pred = request_prediction(MLFLOW_URI, data)[0]
+
+            
+            
+        option = {
+        "tooltip": {
+            "formatter": '{a} <br/>{b} : {c}%'
+        },
+        "series": [{
+            "name": 'Risque de non-solvabilité du client',
+            "type": 'gauge',
+            "startAngle": 180,
+            "endAngle": 0,
+            "progress": {
+                "show": "true"
+            },
+            "radius":'100%',
+            "axisLine": {
+                "lineStyle": {
+                    "width": 6,
+                    "color": [
+                        [0.25,  "#64C88A"],
+                        [0.5, "#FDDD60"],
+                        [0.75, "#ffa500" ],
+                        [1, "#FF403F"],
+                    ],
+                }
+            },
+
+            "itemStyle": {
+                "color": "auto",
+                #"shadowColor": 'rgba(0,138,255,0.45)',
+                "shadowBlur": 10,
+                "shadowOffsetX": 2,
+                "shadowOffsetY": 2,
+                "radius": '55%',
+            },
+            "axisTick": {"length": 10, "lineStyle": {"color": "auto", "width": 2}},
+            "splitLine": {"length": 15, "lineStyle": {"color": "auto", "width": 5}},
+            "progress": {
+                "show": "true",
+                "roundCap": "true",
+                "width": 15
+            },
+            "pointer": {
+                "length": '60%',
+                "width": 8,
+                "offsetCenter": [0, '5%']
+            },
+            "detail": {
+                "valueAnimation": "true",
+                "formatter": '{value}%',
+                "width": '60%',
+                "lineHeight": 20,
+                "fontSize": 30,
+                "height": 20,
+                "borderRadius": 188,
+                "offsetCenter": [0, '40%'],
+            },
+            "data": [{
+                "value": pred,
+                "name": 'Risque de non-solvabilité du client'
+            }]
+        }]
+        };
+
+
+        st.session_state['pred'] = pred
+        col1, col2 = st.columns(2)
+        with col1:
+            st_echarts(options=option, key="0")
+        with col2:
+            if pred > 50:
+                image = Image.open('credit_non.png')
+                st.image(image, caption='', width = 150)
+            else:
+                image = Image.open('credit_oui.png')
+                st.image(image, caption='', width = 150)
+
+            
+            
+        
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ pages/3_Analyse.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import requests
+from datetime import date
+from datetime import datetime, timedelta
+from streamlit_echarts import st_echarts
+import js2py
+from PIL import Image
+import seaborn as sns
+from matplotlib import pyplot as plt
+import lime.lime_tabular
+
+test_data = pd.read_csv("test_df_dropna.csv")
+train_data = pd.read_csv("train_df_dropna.csv")
+if 'data' not in st.session_state:
+    st.session_state['data'] = [train_data.iloc[0][["EXT_SOURCE_1","EXT_SOURCE_2","DAYS_EMPLOYED", "DAYS_BIRTH", "DAYS_ID_PUBLISH", "PAYMENT_RATE", "ANNUITY_INCOME_PERC", "DPD_BOOL"]].values]
+if 'pred' not in st.session_state:
+    st.session_state['pred'] = train_data.iloc[0]["TARGET"]
+    
+def main():
+    MLFLOW_URI = 'http://127.0.0.1:5000/invocations'
+    st.set_page_config(layout="wide")
+
+    feats = ["EXT_SOURCE_1","EXT_SOURCE_2","DAYS_EMPLOYED", "DAYS_BIRTH", "DAYS_ID_PUBLISH", "PAYMENT_RATE", "ANNUITY_INCOME_PERC", "DPD_BOOL"]
+    data = st.session_state['data']
+    pred = st.session_state['pred']
+    data_series = pd.DataFrame(data = np.array(data).reshape(1,-1), columns = feats).iloc[0]
+    if pred > 50 :
+        TARGET_CLIENT = "Non solvable"
+    else :
+        TARGET_CLIENT = "Solvable"
+    COLOR_CLIENT = ["#64C88A" if pred < 25 else "#FDDD60" if pred < 50 else "#ffa500" if pred < 75 else "#FF403F" ][0]
+
+    ANALYSE_OPTION = st.selectbox('Choisissez une option d\'analyse :', ["Explication des résultats", "Graphiques"])
+    if (ANALYSE_OPTION == "Explication des résultats") :
+        target_names = ['credit worthy', 'credit unworthy']
+        classifier_lime = lime.lime_tabular.LimeTabularExplainer(train_data[feats].values,
+                                             mode='classification',
+                                             class_names = target_names,
+                                             categorical_features = [6],
+                                             feature_names= ["Score externe 1", "Score externe 2", "jours travaillés",  "Âge", "Date ID", "Taux de remboursement","Taux d'endettement","Retard de paiements"], discretize_continuous=True, feature_selection = 'auto')
+        MLFLOW_URI = 'http://127.0.0.1:5000/invocations'
+        st.write(type(request_prediction_analyse(data)))
+        st.write(type(data_series[0]))
+        lime_results = classifier_lime.explain_instance(data_series, request_prediction_analyse, num_features=len(feats))
+        lime_resuts_list = lime_results.as_list()
+        st.write(lime_resuts_list)
+    else :
+        subset_0 = train_data[train_data.TARGET == 0][:3000]
+        subset_1 = train_data[train_data.TARGET == 1][:1000]
+        df_subset = pd.concat([subset_0, subset_1])
+        df_subset["DAYS_EMPLOYED"] = abs(df_subset["DAYS_EMPLOYED"])
+        df_subset["TARGET"] = ["Non solvable" if x else "Solvable" for x in df_subset["TARGET"]]
+        DAYS_EMPLOYED_CLIENT = data[0][2]
+        EXT_SOURCE_1_CLIENT = data[0][0]
+        col1, col2 = st.columns(2)
+        with col1:
+            PARAMETRE = st.selectbox('Paramètres à analyser :', ["Données source extérieur 1", "Nombres de jours travaillés"])
+            if PARAMETRE == "Données source extérieur 1" :
+                FEATURE = "EXT_SOURCE_1"
+                FEATURE_CLIENT = EXT_SOURCE_1_CLIENT
+            else :
+                FEATURE = "DAYS_EMPLOYED"
+                FEATURE_CLIENT = DAYS_EMPLOYED_CLIENT
+            fig = plt.figure(figsize=(10, 10))
+            sns.boxplot(data = df_subset, x= "TARGET", y = FEATURE, palette = ["#9ACD32", "#FF0000"])
+            plt.scatter(TARGET_CLIENT, FEATURE_CLIENT, marker='X', s=600, c = COLOR_CLIENT)
+            plt.rcParams.update({'font.size': 24})
+            st.pyplot(fig)
+        with col2 :
+            st.markdown("***")
+            st.markdown("***")
+            sns.set_palette("bright")
+            fig = plt.figure(figsize=(10, 9))
+            sns.scatterplot(data=df_subset, x="EXT_SOURCE_1", y="DAYS_EMPLOYED", hue = "TARGET", palette = ["#9ACD32", "#FF0000"], s = 100)
+            plt.scatter(EXT_SOURCE_1_CLIENT, DAYS_EMPLOYED_CLIENT, marker='X', s=600, c = COLOR_CLIENT)
+            plt.rcParams.update({'font.size': 24})
+            st.pyplot(fig)
+            
+def request_prediction_analyse(data):
+    MLFLOW_URI = 'http://127.0.0.1:5000/invocations'
+    headers = {"Content-Type": "application/json"}
+
+    data_json = {'columns':["EXT_SOURCE_1","EXT_SOURCE_2","DAYS_EMPLOYED", "DAYS_BIRTH", "DAYS_ID_PUBLISH", "PAYMENT_RATE", "ANNUITY_INCOME_PERC", "DPD_BOOL"], 'data': data}
+    response = requests.request(
+        method='POST', headers=headers, url=MLFLOW_URI, json=data_json)
+
+    if response.status_code != 200:
+        raise Exception(
+            "Request failed with status {}, {}".format(response.status_code, response.text))
+            
+    pred_array = np.array([response.json()[0]/100])
+    st.write(pred_array)
+    pred_proba_array = np.array(list(zip(1-pred_array, pred_array))).tolist()
+
+    return pred_proba_array
+    
+    
+
+if __name__ == '__main__':
+    main()
